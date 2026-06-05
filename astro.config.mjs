0e9f4d76c24e342b3232cc063aae8b43683fbd7c
ajreeves <andrew.reeves@gmail.com>
2026-06-04 20:09:25 -0500

    Add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
   },
   integrations: [
     mdx(),
+    sitemap(),
     tailwind({
       applyBaseStyles: false
     })
